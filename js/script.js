fea5aa0101b74b4600044217e06fa6e17e381669
Krzysztof Koniak <czaszkadymi@interia.pl>
2023-04-05 15:22:25 +0200

    function added

--- js/script.js
+++ js/script.js
@@ -1,25 +1,31 @@
-console.log("Siemandero!");
-
-let body = document.querySelector(".body");
-let firstButton = document.querySelector(".js-button__first");
-let themeName = document.querySelector(".js-button__changeText");
-
-firstButton.addEventListener("click", () => {
-  body.classList.toggle("body--dark");
-
-  if (body.classList.contains("body--dark")) {
-    themeName.innerText = "jasne";
-  } else {
-    themeName.innerText = "ciemne";
+{
+  const welcome = () => {
+    console.log("Siemandero!");
   }
-});
 
-let footer = document.querySelector(".footer");
-let SecondButton = document.querySelector(".js-button__second");
-let themeName2 = document.querySelector(".js-button__changeText2");
+  const onChangeBackgroundClick = () => {
+    const body = document.querySelector(".body");
+    const themeName = document.querySelector(".js-button__changeText");
 
-SecondButton.addEventListener("click", () => {
-  footer.classList.toggle("js-footer__hideFooter");
+    body.classList.toggle("body--dark");
+    themeName.innerText = body.classList.contains("body--dark") ? "ciemne" : "jasne";
+  }
 
-  themeName2.innerText = foot.classList.contains("js-footer__hideFooter") ? "Włącz" : "Wyłącz";
-});
+  const onChangeFooterClick = () => {
+    const footer = document.querySelector(".footer");
+    const themeName2 = document.querySelector(".js-button__changeText2");
+    footer.classList.toggle("js-footer__hideFooter");
+    themeName2.innerText = footer.classList.contains("js-footer__hideFooter") ? "Włącz" : "Wyłącz";
+  }
+
+  const init = () => {
+    const firstButton = document.querySelector(".js-button__first");
+    firstButton.addEventListener("click", onChangeBackgroundClick);
+
+    const SecondButton = document.querySelector(".js-button__second");
+    SecondButton.addEventListener("click", onChangeFooterClick);
+    welcome();
+  };
+
+  init();
+}
